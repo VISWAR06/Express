ce7f2640bb4c1ea47bd92e51dc34fdddc84170eb
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-04-30 23:30:49 +0530

    send

--- App.js
+++ App.js
@@ -8,12 +8,14 @@ app.use((req,res,next)=>{
 })
 app.use((req,res,next)=>{
     console.log('2 midwaare')
-    
+    next()
 })
 app.use((req,res,next)=>{
     console.log('3 midwaare')
+    res.send('<h1>bannu payalugah</h1>')
 })
 
 const server=http.createServer(app)
 server.listen(3000)
 // next is used to go to next midware 
+// insd of setheader & wrt use send
