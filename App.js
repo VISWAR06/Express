c55e5465ec900a0976e6fec80db164045b4424bc
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-05-01 00:41:26 +0530

    prefix

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const adminsrout=require('./routes/admin')
 const shopr=require('./routes/Shop')
 app.use(bp.urlencoded())
 app.use(shopr)
-app.use(adminsrout)
+app.use('/admin',adminsrout)
 app.use((req,res,next)=>{
 res.status(404).send('<h1>404 page found </h1>')
 
