0d8b6bf38063193b181bc41c56d7b077dc8610ba
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-05-01 01:08:28 +0530

    404 in router

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ const app=expr()
 const bp=require('body-parser')
 const adminsrout=require('./routes/admin')
 const shopr=require('./routes/Shop')
+const notfound=require('./routes/404')
 app.use(bp.urlencoded())
 app.use(shopr)
 app.use(adminsrout)
-app.use((req,res,next)=>{
-res.status(404).send('<h1>404 page found </h1>')
+app.use(notfound)
+// app.use((req,res,next)=>{
+// res.status(404).send('<h1>404 page found </h1>')
 
-})
+// })
 app.listen(3000)
