95fe234cafe258fcd89681b096fbef6e503f1b13
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-04-30 23:42:21 +0530

    using url

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 const expr=require('express')
 const app=expr()
-const http=require('http')
+// const http=require('http')
+app.use('/second',(req,res,next)=>{
+    console.log('second page')
+    res.send('<h1>second page</h1>')   
+    })
+app.use('/',(req,res,next)=>{
+console.log('first page')
+res.send('<h1>first page</h1>')   
+ }) // always runs so give in second
 
-app.use((req,res,next)=>{
-    console.log('1 midwaare')
-    next()
-})
-app.use((req,res,next)=>{
-    console.log('2 midwaare')
-    next()
-})
-app.use((req,res,next)=>{
-    console.log('3 midwaare')
-    res.send('<h1>bannu payalugah</h1>')
-})
 
-const server=http.createServer(app)
-server.listen(3000)
+
+app.listen(3000)
+// const server=http.createServer(app)
+// server.listen(3000)
 // next is used to go to next midware 
 // insd of setheader & wrt use send
